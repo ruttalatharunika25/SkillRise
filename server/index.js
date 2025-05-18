43811a5571118104bb2c6bd021d7e3f76ba5e061
ruttalatharunika25 <158490271+ruttalatharunika25@users.noreply.github.com>
2025-05-18 16:06:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config({});
 connectDB();
 const app = express();
 
-const PORT = Process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // default middleware
 app.use(express.json());
